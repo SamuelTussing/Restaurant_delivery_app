11f75fe2a7a25daa629dd208bf330dbd75e9c781
Tussing Samuel <sml.tussing@gmail.com>
2023-09-26 17:07:27 +0200

    mise a jour page cart

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,8 +1,44 @@
 import React from "react";
+import Image from "next/image";
 
 const CartPage = () => {
     return (
-        <div>CartPage</div>
+        <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500 lg:flex-row ">
+            {/*  product container */ }
+            <div className="h-1/2 p-4 flex flex-col justify-center overflow-scroll lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40">
+                {/*  single item container */ }
+                <div className="flex items-center justify-between mb-4 ">
+                    <Image src="/temporary/p1.png" alt="" width={100} height={100}/>
+                    <div className="flex flex-col">
+                        <span className="uppercase text-xl font-bold">Sicilian</span>
+                        <span>Large</span>
+                    </div>
+                    <span className="font-bold">€12.50</span>
+                    <span className="cursor-pointer">X</span>
+                </div>
+            </div>
+            {/*  payement container */ }
+            <div className="h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6">
+                <div className="flex justify-between">
+                    <span>Subtotal (3items)</span>
+                    <span>€45.90</span>
+                </div>
+                <div className="flex justify-between">
+                    <span>Service Cost</span>
+                    <span>€45.90</span>
+                </div>
+                <div className="flex justify-between">
+                    <span>Delivery Cost</span>
+                    <span className="text-green-500">Free</span>
+                </div>
+                <div className="flex justify-between">
+                    <span>TOTAL</span>
+                    <span className="text-bold">€100</span>
+                </div>
+                <hr className="my-2"/>
+                <button className="bg-red-500 text-white p-3 rounded-md w-1/2 self-end">CHECKOUT</button>
+            </div>
+        </div>
     )
 }
 export default CartPage
--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,8 +1,29 @@
 import React from "react";
+import { pizzas } from "@/data";
+import Link from "next/link";
+import Image from "next/image";
 
 const CategoryPage = () => {
     return (
-        <div>CategoryPage</div>
+        <div className="flex flex-wrap text-red-500">
+            {pizzas.map(item =>(
+                <Link className='w-full h-[60vh] border-r-2 border-b-2 border-red-500 sm:w-full lg:w-1/3 p-4 flex flex-col justify-between group even:bg-fuchsia-50' href={`/product/${item.id}`} key={item.id}>
+                    {/*  image container  */}
+                     {item.img && (
+                        <div className="relative h-[80%]">
+                            <Image src={item.img} alt="" fill className="object-contain"/>
+                        </div>
+                        )}   
+                    
+                    {/*  text container  */}
+                    <div className="flex items-center justify-between font-bold">
+                        <h2 className="text-2xl uppercase p-2">{item.title}</h2>
+                        <span className="group-hover:hidden text-xl">€{item.price}</span>
+                        <button className=" hidden uppercase group-hover:block bg-red-500 text-white p-2 rounded-md">Ajouter au panier</button>
+                    </div>
+                </Link>
+            ))}
+        </div>
     )
 }
 export default CategoryPage
