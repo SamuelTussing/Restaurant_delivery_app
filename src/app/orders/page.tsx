150de64e9cb291ede1e74ff1670b4a23cd4f125e
Tussing Samuel <sml.tussing@gmail.com>
2023-09-27 10:22:44 +0200

    fin projet

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -2,7 +2,42 @@ import React from "react";
 
 const OrdersPage = () => {
     return (
-        <div>OrdersPage</div>
+        <div className="p-4 lg:px-20 xl:px-40">
+            <table className="w-full border-separate border-spacing-3">
+                <thead>
+                    <tr className="text-left">
+                        <th className="hidden md:block">Order ID</th>
+                        <th>Date</th>
+                        <th>Price</th>
+                        <th className="hidden md:block">Products</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="text-sm md:text-base bg-red-50">
+                        <td className="hidden md:block py-6 px-1">000125200</td>
+                        <td className="py-6 px-1">19.05.2023</td>
+                        <td className="py-6 px-1">€25</td>
+                        <td className="hidden md:block py-6 px-1">Sicilian x2</td>
+                        <td className="py-6 px-1">On the way...</td>
+                    </tr>
+                    <tr className="text-sm md:text-base odd:bg-gray-100">
+                        <td className="hidden md:block py-6 px-1">000125200</td>
+                        <td className="py-6 px-1">19.05.2023</td>
+                        <td className="py-6 px-1">€25</td>
+                        <td className="hidden md:block py-6 px-1">Sicilian x2</td>
+                        <td className="py-6 px-1">Delivered</td>
+                    </tr>
+                    <tr className="text-sm md:text-base odd:bg-gray-100">
+                        <td className="hidden md:block py-6 px-1">000125200</td>
+                        <td className="py-6 px-1">19.05.2023</td>
+                        <td className="py-6 px-1">€25</td>
+                        <td className="hidden md:block py-6 px-1">Sicilian x2</td>
+                        <td className="py-6 px-1">Delivered</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     )
 }
 export default OrdersPage
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Footer = () => {
     return (
-        <div className="h-12 md:h-24 p-4 lg:p20 xl:p-40 text-red-500 flex items-center justify-between">
+        <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between">
             <Link className="font-bold text-xl" href="/">Dolce Vita</Link>
             <p>ALL RIGHTS RESERVED.</p>
         </div>
